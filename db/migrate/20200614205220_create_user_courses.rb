8afb03e20c76d0c1e6f112a1cdb931498065f6e9
Dany-Ocampo <daniela.ocampo@usach.cl>
2023-02-20 20:52:00 -0300

    Error section en Migration

--- db/migrate/20200614205220_create_user_courses.rb
+++ db/migrate/20200614205220_create_user_courses.rb
@@ -2,7 +2,7 @@ class CreateUserCourses < ActiveRecord::Migration[6.0]
   def change
     create_table :user_courses do |t|
       t.belongs_to :user, null: false, foreign_key: true
-      t.belongs_to :section, null: false, foreign_key: true
+      t.belongs_to :course, null: false, foreign_key: true
       t.integer :group_number
 
       t.timestamps

2bd90c9cb76b659eddf84d28adb00b5672b5afb5
Dany-Ocampo <daniela.ocampo@usach.cl>
2023-02-20 21:36:35 -0300

    Application_mailer.rb error

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,5 +2,4 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
 
-  <p><%= link_to 'Unlock my account', unlock_url(@resource, unlock_token: @token, host: @site_host) %></p>
 end
